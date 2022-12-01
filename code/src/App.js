cf0c87f975349a46d169f1e40e3bab9e932a2053
khaliun <khaliun@nest-29s-iMac.local>
2022-12-01 19:06:09 +0800

    ehlel

--- code/src/App.js
+++ code/src/App.js
@@ -1,22 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div className='firstSec'>
+          <div className='head'>
+            <div className='headerContainer'>
+              <div className='netflix'>Netflix</div>
+              <button className='signUp'>Sign in</button>
+            </div>
+          </div>
+          <div></div>
+        </div>
       </header>
     </div>
   );
