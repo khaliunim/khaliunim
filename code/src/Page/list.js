7b70332dd6910e368e47a141de78a4a21fed8252
khaliun <khaliun@nest-29s-iMac.local>
2022-12-10 13:11:50 +0800

    All done

--- code/src/Page/list.js
+++ code/src/Page/list.js
@@ -1,16 +1,15 @@
 import './list.css';  
-import axios from 'axios';
+// import axios from 'axios';
 import { Card } from '../Component/Card';
 import { MOVIE_TREND } from '../Component/trendData';
 import React, { useEffect, useState } from 'react';
 import { MOVIE_LATE } from '../Component/lateData'
 import { Link } from 'react-router-dom';
 
-// export const instance = axios.create()
-
 function List() {
   const [ldata, setLateData] = useState([]);
   const [tdata, setTrendData] = useState([]);
+  const [Valuesearch, setSearch] = useState("")
   const tmovie = MOVIE_TREND;
   const lmovie = MOVIE_LATE;
 
@@ -31,35 +30,38 @@ function List() {
     return (
         <div className='listAllContainer'>
           <div className='header'>
-            <h1 className='title'>Trending</h1>
-            <Link to={"/"}><button className='back'>Home</button></Link>
+            <Link to={"/"}><h1 className='xmovie'>Xmovie</h1></Link>
+            <input placeholder='Enter keywords\...' className='search' value={Valuesearch} onChange={(e)=> setSearch(e.target.value)}></input>
           </div>
 
-          <div className='listContainer'>
-            {
-              tdata.map((cur) => 
-              <Card 
-              img={cur.img}
-              name={cur.name}
-              year={cur.year}
-              time={cur.runtime}
-              id={cur.id}
-              />)
-            }
-          </div>
+            <h1 className='title'>Trending</h1>
+            <div className='listContainer'>
+              {
+                tdata.filter(e => e.name.includes(Valuesearch)).map((cur) =>
+                <Card 
+                img={cur.img}
+                name={cur.name}
+                year={cur.year}
+                time={cur.runtime}
+                id={cur.id}
+                />
+                )
+              }
+            </div>
           <h1 className='title'>Latest Movies</h1>
           <div className='listContainer'>
-            {
-              ldata.map((cur) => 
-              <Card 
-              img={cur.img}
-              name={cur.name}
-              year={cur.year}
-              time={cur.runtime}
-              id={cur.id}
-              />)
-            }
-          </div>
+              {
+                ldata.filter(e => e.name.includes(Valuesearch)).map((cur) =>
+                <Card 
+                img={cur.img}
+                name={cur.name}
+                year={cur.year}
+                time={cur.runtime}
+                id={cur.id}
+                />
+                )
+              }
+            </div>
         </div>
 
     )
